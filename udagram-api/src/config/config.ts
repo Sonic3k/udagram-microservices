0099b70eb3fd3fba30d03a2c2dab7bca5d39fcb8
hypersonic3k <hypersonic3k@gmail.com>
2024-06-23 21:27:08 +0700

    init

---
+++ udagram-api/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': '12345678',
+  'database': 'postgres',
+  'host': 'localhost',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'arn:aws:s3:::udagram-bucket-anhvn1',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'testing',
+  },
+};
